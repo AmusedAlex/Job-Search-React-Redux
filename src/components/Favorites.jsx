3d3bc1eae861a6abb230cab4a3fa877597fe7e14
AmusedAlex <alex.spomer74@gmail.com>
2022-12-01 16:13:08 +0100

    Redux working

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,50 @@
+import { Col, Row, Button, Container } from "react-bootstrap";
+import { House, Trash } from "react-bootstrap-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+// Cart now needs to read and write to the store, reading the value of cart.Data
+// and being able to dispatch an action for removing an element from the cart
+
+const Favorites = ({ cart = [] }) => {
+  const cartData = useSelector((state) => state.cart.data);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  return (
+    <Container style={{ height: "80vh" }}>
+      <Row>
+        <Col sm={12}>
+          <ul style={{ listStyle: "none" }}>
+            {cartData.length > 0 ? (
+              cartData.map((company, i) => (
+                <li key={i} className="my-4 ">
+                  <House className="mr-3" />
+                  <Link to={`/${company.company_name}`}>
+                    {company.company_name}
+                  </Link>
+                  <Button
+                    className="ml-4"
+                    variant="danger"
+                    onClick={() => {
+                      dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: i,
+                      });
+                    }}
+                  >
+                    <Trash />
+                  </Button>
+                </li>
+              ))
+            ) : (
+              <h1 className="text-center mt-5">
+                You have no companies selected
+              </h1>
+            )}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Favorites;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Nav, Navbar } from "react-bootstrap";
+import { HeartFill } from "react-bootstrap-icons";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function NavBar() {
+  const navigate = useNavigate();
+  const cartLength = useSelector((state) => state.cart.data.length);
+  return (
+    <Navbar bg="light" expand="lg">
+      <Link to="/">
+        <Navbar.Brand>Jobs Search Engine</Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"></Nav>
+
+        <Button
+          variant="outline-success"
+          onClick={() => navigate("/favorites")}
+        >
+          <span className="mr-2">View {cartLength} favorite companies</span>{" "}
+          <HeartFill />
+        </Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
